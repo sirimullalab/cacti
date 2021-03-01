e64826a532d421742bb3bec9fa32a8426347e78d
theLife10 <jdgarcia9@miners.utep.edu>
2021-03-01 13:45:45 -0700

    added about

--- src/About.js
+++ src/About.js
@@ -3,18 +3,27 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import MainFeatured from "./MainFeatured";
 import CardPreview from "./CardPreview";
+import Paper from "@material-ui/core/Paper";
+
+const styles = () => ({
+    root: { borderRadius: 20, borderColor: "#000", padding: 50 }
+  });
+  
 
 const About = () => {   
     return (
-    
+        <center>
         <Grid item lg={12}>
+        <Paper>
         <Typography variant="title" component="h1">
             About Us
         </Typography>
         <Typography variant="body2" color="inherit" component="p">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Center for AI-enabled Cancer Therapeutic Innovations" (CACTI) will provide Texas researchers valuable AI resources and expertise for chemotherapeutic discovery, development, and optimization. CACTI will bridge a major gap in Texas’ drug ecosystem and increase the competitiveness of its researchers by providing an integrated environment of leading-edge AI programs and next-generation supercomputer resources capable of successfully translating fundamental cancer biology discoveries into novel cancer drugs. Equally important, CACTI's large team of expert computational chemists will collaborate with Texas researchers and clinicians on innovative chemotherapeutic projects.
         </Typography>
+        </Paper>
         </Grid>
+        </center>
       
     )
   }
